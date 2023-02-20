a30517bdeca4c0699c28490d427ac2ec867d9e94
RYAN MITCHELL <ryan.mitchell@berkeley.edu>
2023-02-19 23:40:23 -0500

    UI improvements for site

--- README.md
+++ README.md
@@ -9,12 +9,23 @@ poetry add black isort pytest httpx
 poetry add diffusers
 poetry add transformers
 poetry add joblib
-
-# move the pipeline.pkl file into the api/ directory
+poetry add kubernetes
 
 # Build container image
-docker build -t capstone_api:1.0 .
+docker build -t capstone_api:1.1 .
 
 # Run container image
-docker run --rm --name capstone_api -p 8000:8000 -d capstone_api:1.0
+docker run --rm --name capstone_api -p 8000:8000 -d capstone_api:1.1
 
+# Push image to dockerhub
+# from mids_capstone_api 
+docker tag capstone_api:1.1 rmitchell88/capstone_api:1.1
+docker login
+docker push rmitchell88/capstone_api:1.1
+
+# On VM terminal
+docker pull rmitchell88/capstone_api:1.1
+docker run --rm --name capstone_api --ipc=host --net=host -v /home/ubuntu:/workspace --gpus=all -d rmitchell88/capstone_api:1.1
+
+# Testing 
+curl -X POST -H "Content-Type: application/json" -d '{"text": "Hello world!"}' http://54.145.236.113:443/generate
--- app.js
+++ app.js
@@ -1,28 +1,32 @@
-  function generateAndDisplayImage(event) {
+function generateAndDisplayImage(event) {
+    event.preventDefault();
     const textInput = document.getElementById('text-input').value;
+    const generatedImage = document.getElementById('generated-image');
+    const loadingSpinner = document.getElementById('loading-spinner');
+    const generateImageButton = document.getElementById('generate-image-button');
+    
+    // Hide the generated image and show the loading spinner
+    generatedImage.style.display = "none";
+    loadingSpinner.style.display = "block";
+    generateImageButton.style.display = "none";
   
-    // // fetch('http://localhost:8000/generate', {
-    // // fetch('http://18.207.245.10:8000/generate', {
-    // // This new IP will use port 443 (HTTPS) by default. Open 443 on EC2 instance.
-    // // This IP is also elastic, so it should remain the same even after restarting EC2 instance. 
-    // // fetch('https://54.145.236.113/generate', {
-    fetch('http://54.145.236.113:443/generate', {    
+    // Send a POST request to generate the image
+    fetch('http://54.145.236.113:443/generate', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json'
       },
       body: JSON.stringify({ text: textInput })
-    //   agent: new https.Agent({
-    //     rejectUnauthorized: false
-    //   })
     })
     .then(response => response.text())
     .then(dataUri => {
-      const image = new Image();
-      image.onload = () => {
-        document.getElementById('generated-image').src = 'data:image/png;base64,' + dataUri;
+      // Swap the loading spinner with the generated image
+      generatedImage.onload = () => {
+        loadingSpinner.style.display = "none";
+        generatedImage.style.display = "block";
+        generateImageButton.style.display = "block";
       };
-      image.src = 'data:image/png;base64,' + dataUri;
+      generatedImage.src = 'data:image/png;base64,' + dataUri;
     })
     .catch(error => console.error('Error generating image:', error));
   }
